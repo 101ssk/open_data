f29d9a448685fc0667580ab5dfeb5220265a5c64
toui <39938112+101ssk@users.noreply.github.com>
2018-12-13 13:10:49 +0900

    Add files via upload

---
+++ my-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: my-plugin
+Plugin URI:
+Description: My Plugin For WordPress.
+Version: 1.0.0
+Author:
+Author URI:
+License: GPLv2 or later
+*/
+
+
+// ここからコードをいれてください。
+/**
+ * GoogleMaps jsを追加 footer
+ *
+ * License: GPLv2 or later
+ */
+function add_footer_enqueue_scripts_googlemaps_js() {
+    wp_enqueue_script( 'googlemapapi', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBSOskstNrYcYvY1zMkqE8cAA0TH9G2Sbc', array(), '', true );
+}
+add_action( 'wp_enqueue_scripts', 'add_footer_enqueue_scripts_googlemaps_js', 99 );
